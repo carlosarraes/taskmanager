e1cc136c3f6003a70acd31d78c28327bd54936cb
carraes <carraeshb@gmail.com>
2022-10-24 16:14:28 -0300

    build: set up routes

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Task Manager App')
+const colors = require('colors');
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks')
+
+const PORT = 3000;
+
+//* Middleware
+app.use(express.json())
+app.use(express.static('./public'))
+
+//? Routes
+app.use('/api/v1/tasks', tasks)
+
+app.listen(PORT, console.log(`Listening on: http://localhost:${PORT}/`.cyan))
