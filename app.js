5dc849c91b2311cde80cffed65f45ebea24b1711
carraes <carraeshb@gmail.com>
2022-10-27 17:22:54 -0300

    build: setup mongoose

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const colors = require('colors');
 const express = require('express');
 const app = express();
-const tasks = require('./routes/tasks')
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
 
 const PORT = 3000;
 
 //* Middleware
-app.use(express.json())
-app.use(express.static('./public'))
+app.use(express.json());
+app.use(express.static('./public'));
 
 //? Routes
-app.use('/api/v1/tasks', tasks)
+app.use('/api/v1/tasks', tasks);
 
-app.listen(PORT, console.log(`Listening on: http://localhost:${PORT}/`.cyan))
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL)
+    console.log('[MongoDB] started'.brightGreen.bold);
+    app.listen(PORT, console.log(`Listening on: http://localhost:${PORT}/`.brightCyan.bold));
+  } catch (error) {
+    console.log((error.message).red.bold);
+  }
+};
+
+start();
