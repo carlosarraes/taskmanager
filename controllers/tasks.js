1e07a4d8af2837e96427ea2f7c83e70485c35bfc
carraes <carraeshb@gmail.com>
2022-11-02 22:43:38 -0300

    build: creat tasks

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,12 +1,21 @@
 const Task = require('../models/Task')
 
-const getAllTasks = (req,res)=>{
-  res.send('get all items')
+const createTask = async (req,res)=>{
+  try {
+    const task = await Task.create(req.body)
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
 }
 
-const createTask = async (req,res)=>{
-  const task = await Task.create(req.body)
-  res.status(201).json({ task });
+const getAllTasks = async (req,res)=>{
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
 }
 
 const getTask = (req,res)=>{
